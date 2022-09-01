da0d4db143f56b045d5491b3b5e9c1d6fa7747a8
mohantalachutla <mohan.talachutla@gmail.com>
2022-09-01 15:16:08 +0530

    Tests

--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,28 @@
-import { add } from './app'
+import http from './http'
+import dotenv from 'dotenv'
+import dotenvExpand from 'dotenv-expand'
+dotenvExpand.expand(dotenv.config())
 
-describe('App tests', () => {
-  it('has to be 2', () => {
-    expect(add()).toBe(2)
+const appName = process.env.APP_NAME
+const host = process.env.HOST
+const port = process.env.PORT
+
+describe('Test Environment ', () => {
+  it(`Has ENV APP_NAME set`, () => {
+    expect(appName).toBeDefined()
+  })
+
+  it(`Has ENV HOST set`, () => {
+    expect(host).toBeDefined()
+  })
+
+  it(`Has ENV PORT set`, () => {
+    expect(port).toBeDefined()
+  })
+
+  it(`has to send reponse ${appName}`, async () => {
+    expect(await (await http.get(`http://${host}:${port}/`)).data).toContain(
+      appName
+    )
   })
 })
